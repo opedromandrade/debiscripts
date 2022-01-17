abbdf56150d44f1d8921110a147fd6e47d46ee1a
pedro andrade <43356335+opedromandrade@users.noreply.github.com>
2022-01-17 22:12:13 +0000

    Update debian.sh

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -2,28 +2,31 @@
 # 
 # Author: Pedro Andrade - https://github.com/opedromandrade
 #
-# updated on: 09.05.2021
+# updated on: 17.01.2022
 #
 # Let's roll!
 
-# Remove stuff
-sudo apt remove gnome-2048 gnome-klotski gnome-mines gnome-robots gnome-sudoku gnome-taquin gnome-tetravex four-in-a-row hitori iagno lightsoff gnome-music quadrapassel tali transmission-gtk
+# Remove stuff - if on Gnome
+#sudo apt remove gnome-2048 gnome-klotski gnome-mines gnome-robots gnome-sudoku gnome-taquin gnome-tetravex four-in-a-row hitori iagno lightsoff gnome-music quadrapassel tali transmission-gtk
 
 ## Debian Multimedia
 # Get that key
-wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
+#wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
 # Make magic
-sudo dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
+#sudo dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
 
 # Update system
 sudo apt update && sudo apt upgrade
 
 # Install Intel proprietary stuff
-sudo apt-get install i965-va-driver-shaders intel-media-va-driver-non-free intel-gpu-tools
+#sudo apt-get install i965-va-driver-shaders intel-media-va-driver-non-free intel-gpu-tools
+
+# Activate touchpad tap2click
+sudo apt-get install xdotool wmctrl libinput-tools
 
 ## Small things
 # Open JDK
-sudo apt-get install openjdk-17-jre
+#sudo apt-get install openjdk-17-jre
 
 # Git
 sudo apt-get install git
@@ -42,11 +45,11 @@ sudo tlp start
 
 ## VPN stuff
 # Get wireguard
-sudo apt-get install wireguard
+#sudo apt-get install wireguard
 
 ## Office tools
 # LibreOffice
-sudo apt-get install libreoffice-gnome libreoffice
+sudo apt-get install libreoffice
 
 # LibreOffice Portuguese localization et all
 sudo apt-get install myspell-pt-pt hyphen-pt-pt libreoffice-l10n-pt mythes-pt-pt libreoffice-help-pt
@@ -77,7 +80,7 @@ sudo apt-get install easytag
 sudo apt-get install sigil
 
 # Calibre
-sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+sudo apt-get install calibre
 
 ## Aditional formats for compression
 sudo apt-get install unace rar zip unzip p7zip p7zip-full p7zip-rar sharutils uudeview arj cabextract
@@ -99,14 +102,14 @@ sudo apt-get install kdenlive
 # Remove KDE Connect
 sudo apt remove kdeconnect
 
-# Openshot
-#sudo apt-get install openshot
+# ShotCut
+sudo apt-get install shotcut
 
 # Audacity
 sudo apt-get install audacity
 
 #Simple Screen Recorder
-#sudo apt-get install simplescreenrecorder
+sudo apt-get install simplescreenrecorder
 
 ## Internet Stuff
 # Extra browser Chromium and some extra stuff. For more info: https://wiki.debian.org/Chromium#Drivers_and_libraries_according_to_your_hardware
@@ -123,13 +126,13 @@ sudo apt-get install gufw
 
 ## Beautify
 # Beautify tools
-sudo apt-get install chrome-gnome-shell gedit-plugin-text-size gnome-tweak-tool
+#sudo apt-get install chrome-gnome-shell gedit-plugin-text-size gnome-tweak-tool
 
 # Set Adwaita Dark
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 
 # NUMiX
-sudo apt-get install numix-gtk-theme numix-icon-theme numix-icon-theme-circle
+#sudo apt-get install numix-gtk-theme numix-icon-theme numix-icon-theme-circle
 
 # Clean some more [just for reinsurance]
 sudo apt autoremove && sudo apt autoclean
