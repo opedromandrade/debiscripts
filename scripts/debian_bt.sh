0375269655de59c83e1e5c3943443d3c8c510a14
pedro andrade <43356335+opedromandrade@users.noreply.github.com>
2022-01-18 22:18:35 +0000

    Add files via upload
    Bluetooth install sript

---
+++ scripts/debian_bt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 
+# This script installs and enables bluetooth ona fresh installed Debian system. After install, it's advised to reboot.
+# Author: Pedro Andrade - https://github.com/opedromandrade
+# updated on: 18.01.2022
+
+sudo apt-get install blueman -y
+
+sudo systemctl enable bluetooth \
+
+sudo systemctl enable --now bluetooth
+
+# Gotta reboot now
+echo $'\n'$"*** Follow the white rabbit & reboot ***"
+exit
